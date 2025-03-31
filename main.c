b71ccf288555c4e371a10789ebec3933c6a2092c
Pedro Garcia Vilanova <pedrovilanova2@hotmail.com>
2025-03-31 14:06:28 -0300

    Configuração inicial do makefile

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "helpers/vector.h"
+#include "helpers/buffer.h"
+
+int main(int argc, char *argv[], char *envp[]) {
+    printf("Teste de compilação!");
+
+
+    return 0;
+
+    /* Testes das funções helpers */
+    //
+}
